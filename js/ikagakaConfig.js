e591b86ce6912b0c8a8a2b1fd6bc3b5864705fde
rowgetu <rowgetu@gmail.com>
2025-05-23 10:28:17 +0900

    Update ikagakaConfig.js

--- js/ikagakaConfig.js
+++ js/ikagakaConfig.js
@@ -42,7 +42,7 @@ var ikagakaConfig = {
     /**
      * 最初に起動するバルーンディレクトリ名（＝install.txtのdirectoryエントリ名）
      */
-    initialBalloonpath: "meganeoni",
+    initialBalloonpath: null,
     /**
      * 初期にインストールするnarのパス配列
      *
